ec6f7ff8a34b4c16eeb3b7a7d560e70275054a2e
Lokendra Rajpurohit <rajpurohitlokendra62@gmail.com>
2025-11-29 20:48:25 +0530

    little change

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
-const projectName = process.argv[2] || 'my-mern-app';
+const projectName = 'create-mern-boiler';
 
 console.log(`🚀 Creating MERN app: ${projectName}`);
-execSync(`git clone https://github.com/lokendra0001/my-mern-app ${projectName}`, { stdio: 'inherit' });
+execSync(`git clone https://github.com/Lokendra0001/mern-boiler-template ${projectName}`, { stdio: 'inherit' });
 
 console.log(`✅ Done! Now run:
   cd ${projectName}
